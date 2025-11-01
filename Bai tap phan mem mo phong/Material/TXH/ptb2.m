32cce1bf79f88047870dccd3c068a5c4e5872089
XutBeTi <traan.hoan.vn@gmail.com>
2025-11-01 10:19:50 +0700

    update

--- Bai tap phan mem mo phong/Material/TXH/ptb2.m
+++ Bai tap phan mem mo phong/Material/TXH/ptb2.m
@@ -1,50 +1,81 @@
 clear;
 clc;
 
-grid on;
-hold on;
-
 a = input('Nhap gia tri cho bien a: ');
 b = input('Nhap gia tri cho bien b: ');
 c = input('Nhap gia tri cho bien c: ');
- 
-delta=b*b-4*a*c;
- 
-if (delta<0)
-    
-    disp('Phuong trinh vo nghiem');
-    x1=-b/(2*a);
-     
-    x = x1-3:0.01:x1+3;
-    
-elseif (delta==0)
-    x1=-b/(2*a);
-    disp (['phuong trinh co 1 nghiem kep x= ', num2str(x1)]);
-    x= x1-3:0.01:x1+3;
-     
-    plot ( [x1],[0], 'ro' , 'markersize' , 8, 'markerfacecolor', 'r' );
-      
-else
-    x1=(-b-sqrt(delta))/(2*a);
-    x2=(-b+sqrt(delta))/(2*a);
-    disp (['phuong trinh co 2 nghiem x1= ', num2str(x1),' va x2= ', num2str(x2)]);
-     
-    x= x1-3:0.01:x2+3;
-    
-    plot ( [x1 x2],[0 0], 'ro' , 'markersize' , 8, 'markerfacecolor', 'r' );
+
+if a == 0
+    if b == 0
+        if c == 0
+            disp('Phuong trinh vo so nghiem (0 = 0)');
+            x = c - 5 : 0.01 : c + 5;
+        else
+            disp('Phuong trinh vo nghiem ( Const != 0)');
+            x = c - 5 : 0.01 : c + 5;
+        end
+
+    else
+        % Phuong trinh bac nhat: bx + c = 0
+        x = -c/b;
+        disp(['Phuong trinh co 1 nghiem x = ', num2str(x)]);
+
+        plot ( [x],[0], 'ro' , 'markersize' , 8, 'markerfacecolor', 'r' );
+
+        x = x - 5 : 0.01 : x + 5;
         
+       
+    end
+else 
+
+    delta=b*b-4*a*c;
+     
+    if (delta<0)
+        
+        disp('Phuong trinh vo nghiem');
+        x1=-b/(2*a);
+        x = x1-3:0.01:x1+3;
+        
+    elseif (delta==0)
+        x1=-b/(2*a);
+        disp (['phuong trinh co 1 nghiem kep x= ', num2str(x1)]);
+        x= x1-3:0.01:x1+3;
+        plot ( [x1],[0], 'ro' , 'markersize' , 8, 'markerfacecolor', 'r' );
+          
+    else
+        x1=(-b-sqrt(delta))/(2*a);
+        x2=(-b+sqrt(delta))/(2*a);
+        disp (['phuong trinh co 2 nghiem x1= ', num2str(x1),' va x2= ', num2str(x2)]);
+
+        if (x1>x2)
+            temp=x2;
+            x2=x1;
+            x1=temp;
+        end 
+
+        space=x2-x1; 
+
+        x= x1-space*0.2 : 0.01 : x2+space*0.2;
+
+        plot ( [x1 x2],[0 0], 'ro' , 'markersize' , 8, 'markerfacecolor', 'r' );
+            
+    end
+     
 end
- 
- hold on;
- 
- y = a*x.^2 + b*x + c;   
- plot(x, y, 'b-', 'LineWidth', 2);
- 
-   
-  
- hold on;
- 
- y=x.*0;
- 
- plot( x  , y , 'k-', 'LineWidth', 2);
- 
+
+hold on;   
+
+grid on;
+
+hold on; 
+
+y = a*x.^2 + b*x + c;   
+plot(x, y, 'k-', 'LineWidth', 2);
+
+hold on;
+
+y=x.*0;
+plot( x  , y , 'k-', 'LineWidth', 1);
+
+hold on;
+    

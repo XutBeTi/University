3783761e682b63439741c6198d2d8271efef6ddf
XutBeTi <traan.hoan.vn@gmail.com>
2025-11-01 11:50:51 +0700

    none

--- Bai tap phan mem mo phong/Material/TXH/bai1.m
+++ Bai tap phan mem mo phong/Material/TXH/bai1.m
@@ -1,10 +1,12 @@
 clear;
 clc;
+close all;
 
 a = input('Nhap gia tri cho bien a: ');
 b = input('Nhap gia tri cho bien b: ');
 c = input('Nhap gia tri cho bien c: ');
 
+% Xử lí đầu vào không phải phương trình bậc 2
 if a == 0
     if b == 0
         if c == 0
@@ -14,40 +16,40 @@ if a == 0
             disp('Phuong trinh vo nghiem ( Const != 0)');
             x = c - 5 : 0.01 : c + 5;
         end
-
     else
-        % Phuong trinh bac nhat: bx + c = 0
+
+        % Phương trình bậc nhất: bx + c = 0
         x = -c/b;
         disp(['Phuong trinh co 1 nghiem x = ', num2str(x)]);
-
         plot ( [x],[0], 'ro' , 'markersize' , 8, 'markerfacecolor', 'r' );
-
+        text(x-sign(b)*0.3 , -sign(b)*0.5,['(',num2str(x),',',num2str(0),')']);
         x = x - 5 : 0.01 : x + 5;
-        
-       
     end
 else 
-
+    % Phương trình bậc 2
     delta=b*b-4*a*c;
      
-    if (delta<0)
-        
+    if (delta<0)   
         disp('Phuong trinh vo nghiem');
         x1=-b/(2*a);
         x = x1-3:0.01:x1+3;
-        
+
     elseif (delta==0)
         x1=-b/(2*a);
         disp (['Phuong trinh co 1 nghiem kep x= ', num2str(x1)]);
         x= x1-3:0.01:x1+3;
         plot ( [x1],[0], 'ro' , 'markersize' , 8, 'markerfacecolor', 'r' );
+
+        text(x1 , -sign(a) ,['(',num2str(x1),',',num2str(0),')'],'horizontalalignment','center');
         hold on;
+
+        % Thêm 1 điểm trùng với màu nền để đưa trục hoành cách cạnh đồ thị 
         plot ( [x1],[-(a*5)], 'wo' , 'markersize' , 1, 'markerfacecolor', 'w' );
           
     else
         x1=(-b-sqrt(delta))/(2*a);
         x2=(-b+sqrt(delta))/(2*a);
-        disp (['Phuong trinh co 2 nghiem x1= ', num2str(x1),' va x2= ', num2str(x2)]);
+        disp (['Phuong trinh co 2 nghiem x1= ',num2str(x1),' va x2= ', num2str(x2)]);
 
         if (x1>x2)
             temp=x2;
@@ -55,27 +57,27 @@ else
             x1=temp;
         end 
 
+        text(x1, -sign(a) ,['(',num2str(x1),',',num2str(0),')'],'horizontalalignment','right');
+        hold on;
+        text(x2 , -sign(a) ,['(',num2str(x2),',',num2str(0),')'],'horizontalalignment','left');
+        hold on;
+
         space=x2-x1;
         x= x1-space*0.3 : 0.01 : x2+space*0.3;
-        plot ( [x1 x2],[0 0], 'ro' , 'markersize' , 8, 'markerfacecolor', 'r' );
-            
+        plot ( [x1 x2],[0 0], 'ro' , 'markersize' , 8, 'markerfacecolor', 'r' );  
     end
-     
 end
 
+% Vẽ đồ thị và trục hoành
 hold on;   
-
 grid on;
 
 hold on; 
-
 y = a*x.^2 + b*x + c;   
 plot(x, y, 'k-', 'LineWidth', 2);
 
 hold on;
-
 y=x.*0;
-plot( x  , y , 'k--', 'LineWidth', 1);
+plot( x  , y , 'k--', 'LineWidth', 1);   
 
-hold on;
-    
+title (['Đồ thị phương trình ',num2str(a), 'x^2 + ' , num2str(b), 'x + ' ,num2str(c) ]);
---
+++ Bai tap phan mem mo phong/Material/TXH/bai2.m
@@ -0,0 +1,55 @@
+clear;
+clc;
+close all;
+
+% Nhập vận tốc ban đầu (m/s)
+v0 = input('Nhap van toc ban dau (m/s): '); 
+
+% Độ cao ban đầu (m)
+y0 = 10; 
+alpha_rad = deg2rad(45);
+
+% Gia tốc trọng trường (m/s^2)
+g = 9.8; 
+
+% Tính toán thời gian vật chạm đất (y = 0).
+% Chọn chiều dương hướng xuống
+% Giải phương trình bậc 2 theo t: (1/2)gt^2 - (v0*sin(alpha))*t - y0 = 0
+% a = 0.5*g; b = -v0*sin(alpha_rad); c = -y0;
+
+delta = (-v0*sin(alpha_rad))^2 - 4 * (0.5*g) * (-y0);
+
+% Thời gian chạm đất (nghiệm dương phương trình bậc 2) 
+t_max = (-(-v0*sin(alpha_rad)) + sqrt(delta)) / (2 * (0.5*g));
+
+% Tạo vector thời gian
+t = 0 : 0.01 : t_max; % Vector thời gian từ 0 đến khi chạm đất
+
+% Vị trí theo trục ngang
+x = v0 * cos(alpha_rad) * t;
+
+% Vị trí theo trục dọc
+y = y0 + v0 * sin(alpha_rad) * t - 0.5 * g * t.^2;
+
+plot(x, y, 'k-', 'LineWidth', 2); % Vẽ quỹ đạo bằng nét liền màu đen
+hold on;
+grid on;
+
+% Đánh dấu điểm bắt đầu (10m)
+plot(0, y0, 'ro', 'MarkerSize', 8, 'MarkerFaceColor', 'r'); 
+text( 0 , y0 -1, 'Diem nem (10m)', 'HorizontalAlignment', 'left');
+
+% Đánh dấu điểm chạm đất
+plot(x(end), 0 , 'rx', 'MarkerSize', 10, 'LineWidth', 2);
+text(x(end)* 0.95, 1 , 'Cham dat', 'HorizontalAlignment', 'right');
+
+plot(x(end)*1.1, 0 , 'wx', 'MarkerSize', 1);
+
+% Vẽ trục hoành y=0 (mặt đất)
+plot([min(x) x(end)], [0 0], 'k--', 'LineWidth', 1);
+
+% Thiết lập nhãn và tiêu đề
+xlabel('Khoang cach ngang (m)');
+ylabel('Do cao (m)');
+title(['Quy dao vat nem xien (v0 = ', num2str(v0), ' m/s, goc = ', num2str(45), ' do)']);
+axis equal tight; % Đảm bảo tỷ lệ trục hợp lý và vừa khít dữ liệu
